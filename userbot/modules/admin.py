1c621c45b9b6e960f4478ee8ce693ef7efe042d2
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-03-17 20:59:09 -0700

    zombie ke indo3

--- userbot/modules/admin.py
+++ userbot/modules/admin.py
@@ -536,7 +536,7 @@ async def rm_deletedacc(show):
                 del_u += 1
                 await sleep(1)
         if del_u > 0:
-            del_status = f"`Ditemukan` **{del_u}** `Akun Sampah Pada Grup Ini ,\
+            del_status = f"`Ditemukan` **{del_u}** `Akun Sampah Pada Grup Ini,\n
             \Bersihkan Dengan Perintah .zombies clean`"
         await show.edit(del_status)
         return
