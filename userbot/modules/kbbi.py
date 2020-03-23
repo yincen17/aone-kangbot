93b9709c840f6b505359cf92a5a1ddf6e42e4395
yincen17 <52388255+yincen17@users.noreply.github.com>
2020-03-22 18:46:08 -0700

    mencoba mengatasi masalah import

--- userbot/modules/kbbi.py
+++ userbot/modules/kbbi.py
@@ -6,7 +6,7 @@ from kbbi import KBBI
 from pyrogram import Filters
 
 # from nana import app, Command
-from nana import app, Command
+from userbot import app, Command
 
 __MODULE__ = "KBBI"
 __HELP__ = """
